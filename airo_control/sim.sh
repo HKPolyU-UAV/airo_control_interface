ae96b14f8e5171ec04e4471fd7485b745a70942a
18049261d@connect.polyu.hk <you@example.com>
2023-12-01 17:02:25 +0800

    commit

--- airo_control/sim.sh
+++ airo_control/sim.sh
@@ -1,6 +1,6 @@
 cd ~/PX4-Autopilot
 DONT_RUN=1 make px4_sitl_default gazebo
-source /home/athena/airo_control_interface_ws/devel/setup.bash   # (optional)
+source ~/catkin_ws/devel/setup.bash   # (optional)
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
