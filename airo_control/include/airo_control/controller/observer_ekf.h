8961966c52fc0ee66ea2136eae8174bb948dbf9d
18049261d@connect.polyu.hk <you@example.com>
2024-01-04 11:52:19 +0800

    commit

--- airo_control/include/airo_control/controller/observer_ekf.h
+++ airo_control/include/airo_control/controller/observer_ekf.h
@@ -6,7 +6,6 @@
 #include <ros/ros.h>
 #include <tf/tf.h>
 #include <eigen3/Eigen/Dense>
-#include <gazebo_msgs/ApplyBodyWrench.h>
 
 #include "acados_solver_quadrotor.h"
 
@@ -47,11 +46,51 @@ class OBSERVER_EKF: public BASE_CONTROLLER{
             psi_cmd = 3,
         };
 
-        struct SolverInput{
-            double x0[QUADROTOR_NX];
-            double yref[QUADROTOR_N+1][QUADROTOR_NY];
+        // struct SolverInput{
+        //     double x0[QUADROTOR_NX];
+        //     double yref[QUADROTOR_N+1][QUADROTOR_NY];
+        // };
+
+        // struct SolverOutput{
+        //     double u0[QUADROTOR_NU];
+        //     double x1[QUADROTOR_NX];
+        //     double status,kkt_res, cpu_time;
+        // };
+
+        struct Euler{
+            double phi;
+            double theta;
+            double psi;
         };
 
+        struct Pos{
+            double x;
+            double y;
+            double z;
+            double u;  // dx
+            double v;  // dy
+            double w;  // dz
+            double p;  // dphi
+            double q;  // dtheta
+        };
+
+        struct Acc{
+            double x;  // du
+            double y;  // dv
+            double z;  // dw
+        };
+
+        struct SolverParam{
+            double disturbance_x;
+            double disturbance_y;
+            double disturbance_z;
+        };
+
+        struct Thrust{
+            double thrust_1;  // U1 in du
+            double thrust_2;  // U1 in dv
+            double thrust_3;  // U1 in dw
+        };
 
     public:
         Param param;
