9658ca47b4aab9b76ac82e5ebee02cde118e6037
Vitollee <ace0lee@gmail.com>
2024-01-24 21:59:11 +0800

    new

--- airo_control/src/controller/base_controller.cpp
+++ airo_control/src/controller/base_controller.cpp
@@ -1,4 +1,6 @@
 #include "airo_control/controller/base_controller.h"
+#include <cmath> 
+#include <iostream>
 
 Eigen::Vector3d BASE_CONTROLLER::q2rpy(const geometry_msgs::Quaternion& quaternion){
     tf::Quaternion tf_quaternion;
@@ -13,16 +15,40 @@ geometry_msgs::Quaternion BASE_CONTROLLER::rpy2q(const Eigen::Vector3d& euler){
     return quaternion;
 }
 
-float BASE_CONTROLLER::inverse_thrust_model(const double& a_z,const float& voltage,const Param& param,const ThrustModel& thrust_model){
+float BASE_CONTROLLER::inverse_thrust_model(const double& a_z,const float& voltage,const Param& param,const ThrustModel& thrust_model)
+{
     float thrust;
+    float voltage_2 = 14.8; // 4s battery fully charged
 
     if (param.enable_thrust_model) {
-        thrust = (a_z/g)*param.hover_thrust;
-        std::cout<<"under development"<<std::endl;
+        
+    const float g = 9.81;  // Acceleration due to gravity
+            // thrust = (a_z/g)*param.hover_thrust;
+            //  std::cout<<a_z<<std::endl;
+
+        double Th = (sqrt(((thrust_model.mass * a_z)/ 4) / (thrust_model.K1 * pow(voltage, thrust_model.K2))) + pow((1 - thrust_model.K3) / (2 * sqrt(thrust_model.K3)), 2) - ((1 - thrust_model.K3) / (2 * sqrt(thrust_model.K3)))) / sqrt(thrust_model.K3);
+   
+        thrust = Th ; 
+
+        // std::cout<<"enable thrust_model"<<std::endl;
+        std::cout<<voltage<<std::endl;
+
+    //       if (thrust > 1.0) {
+    //     ROS_ERROR("Thrust = %f",thrust);
+    //     thrust = 0.9999;
+    // }
+    //      else if (thrust < 0.0) {
+    //     ROS_ERROR("Thrust = %f",thrust);
+    //     thrust = 0.0001;
+    // }
+
+    //     std::cout<<"Thrust = %f"<<std::endl;
+    
+    return thrust;
     }
     else {
         thrust = (a_z/g)*param.hover_thrust;
-    }
+    } 
 
     if (thrust > 1.0) {
         ROS_ERROR("Thrust = %f",thrust);
