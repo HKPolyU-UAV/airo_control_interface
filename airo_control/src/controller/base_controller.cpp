602d6479d03781e92871b9ffebc03059ffc3ea98
Vitollee <ace0lee@gmail.com>
2024-01-30 08:33:46 +0000

    new

--- airo_control/src/controller/base_controller.cpp
+++ airo_control/src/controller/base_controller.cpp
@@ -18,47 +18,33 @@ geometry_msgs::Quaternion BASE_CONTROLLER::rpy2q(const Eigen::Vector3d& euler){
 float BASE_CONTROLLER::inverse_thrust_model(const double& a_z,const float& voltage,const Param& param,const ThrustModel& thrust_model)
 {
     float thrust;
-    float voltage_2 = 14.8; // 4s battery fully charged
+    float voltage_2 = 16.8; // 4s battery fully charged
 
     if (param.enable_thrust_model) {
-    
-            thrust = (a_z/g)*param.hover_thrust;
-            // std::cout<<a_z<<std::endl;
-            // std::cout<<thrust<<std::endl;
 
-        double thrust = (sqrt(((thrust_model.mass * a_z)/ 4) / (thrust_model.K1 * pow(voltage, thrust_model.K2)) + pow((1 - thrust_model.K3) / (2 * sqrt(thrust_model.K3)), 2)) - ((1 - thrust_model.K3) / (2 * sqrt(thrust_model.K3)))) / sqrt(thrust_model.K3);
+            
+            // thrust = (sqrt(((1 * a_z)/4) / (K1 * pow(voltage_2, K2)) + pow(((1 - K3) / (2 * sqrt(K3))), 2)) - ((1 - K3) / (2 * sqrt(K3)))) / sqrt(K3);
+            double thrust = (sqrt(((thrust_model.mass * a_z)/ 4) / (thrust_model.K1 * pow(voltage, thrust_model.K2)) + pow((1 - thrust_model.K3) / (2 * sqrt(thrust_model.K3)), 2)) - ((1 - thrust_model.K3) / (2 * sqrt(thrust_model.K3)))) / sqrt(thrust_model.K3);
  
-        std::cout<<a_z<<std::endl;
-        // std::cout<<"enable thrust_model"<<std::endl;
+        // std::cout<<a_z<<std::endl;
+        std::cout<<"enable thrust_model"<<std::endl;
         std::cout<<thrust<<std::endl;
-        std::cout<<voltage<<std::endl;
-        
-          if (thrust > 1.0) {
-        // std::cout<<"OVER!!!"<<std::endl;
-        // ROS_ERROR("Thrust = %f",thrust);
-        thrust = 0.9999;
-    }
-         else if (thrust < 0.0) {
-        // ROS_ERROR("Thrust = %f",thrust);
-        thrust = 0.0001;
-    }
-
-        // std::cout<<"Thrust = %f"<<std::endl;
-    
 
     }
     else {
         thrust = (a_z/g)*param.hover_thrust;
+        std::cout<<a_z<<std::endl;
+        std::cout<<thrust<<std::endl;
     } 
 
-    if (thrust > 1.0) {
-        ROS_ERROR("Thrust = %f",thrust);
-        thrust = 0.9999;
-    }
-    else if (thrust < 0.0) {
-        ROS_ERROR("Thrust = %f",thrust);
-        thrust = 0.0001;
-    }
+    // if (thrust > 1.0) {
+    //     ROS_ERROR("Thrust = %f",thrust);
+    //     thrust = 0.9999;
+    // }
+    // else if (thrust < 0.0) {
+    //     ROS_ERROR("Thrust = %f",thrust);
+    //     thrust = 0.0001;
+    // }
 
     return thrust;
 }
