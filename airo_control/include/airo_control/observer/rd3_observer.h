4345455406a43ffa5eff678ab61b5b72f2e84fc4
RockyJBL <jiangbailun@gmail.com>
2024-05-17 21:59:44 +0800

    minor update

--- airo_control/include/airo_control/observer/rd3_observer.h
+++ airo_control/include/airo_control/observer/rd3_observer.h
@@ -5,7 +5,11 @@
 
 class RD3 : public BASE_OBSERVER{
     protected:
+        struct Param{
+            double k1,k2,k3;
+        };
 
+        Param param;
     public:
         RD3(ros::NodeHandle&);
         geometry_msgs::AccelStamped observe(const geometry_msgs::PoseStamped&, const geometry_msgs::TwistStamped&, const geometry_msgs::AccelStamped&, const double&);
--- airo_control/src/observer/rd3_observer.cpp
+++ airo_control/src/observer/rd3_observer.cpp
@@ -1,9 +1,9 @@
 #include "airo_control/observer/rd3_observer.h"
 
 RD3::RD3(ros::NodeHandle& nh){
-
+    nh.getParam("airo_control_node/rd3_observer/k1",param.k1);
 }
 
-geometry_msgs::AccelStamped RD3::observe(const geometry_msgs::PoseStamped&, const geometry_msgs::TwistStamped&, const geometry_msgs::AccelStamped&, const double&){
+geometry_msgs::AccelStamped RD3::observe(const geometry_msgs::PoseStamped&, const geometry_msgs::TwistStamped&, const geometry_msgs::AccelStamped&, const double& ){
     
 }
